88a8d6bc73d5a2008dc44fbb4acb95e3e2c50303
Daniel Garden <dangarden@gmail.com>
2018-05-18 21:34:33 +1000

    meta tag title

--- external_header.php
+++ external_header.php
@@ -7,10 +7,12 @@
     if(!isset($page["image"])) {
         $page["image"] = 'http://' . $_SERVER['HTTP_HOST'] . '/assets/site-header.png';
     }
+
+    $page["name"] = "VZROH.com &raquo; " . $page["name"];
 ?>
 <meta charset="utf-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<title>VZROH.com &raquo; <?php echo $page['name'];?></title>
+<title><?php echo $page['name'];?></title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
